970867fdd4bca4546bfd3544d01e1b4035c4375c
metachan-golf <takuzou@metachan.jp>
2026-06-21 08:32:01 +0900

    Update website files

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 // 川島第四町内会 PWA Service Worker
-// Version: 2026-06-21-20260621_073500
+// Version: 2026-06-21-20260621_064700
 // HTMLは最新情報を優先し、画像・アイコン等は軽くキャッシュします。
-// Update Note: トップページ主要メニューのPC表示調整に伴いキャッシュ名を更新。
+// Update Note: トップページ最終更新日の修正に伴いキャッシュ名を更新。
 
-const CACHE_NAME = 'kawashima-d4-pwa-20260621_073500';
+const CACHE_NAME = 'kawashima-d4-pwa-20260621_074500';
 const STATIC_ASSETS = [
   './',
   './index.html',
